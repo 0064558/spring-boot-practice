8745ee32e7f6335ae6ee8ed12a7110010b7e3967
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-01 16:11:43 -0300

    feat: Add findById and createEmployee methods to EmployeeController

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/controllers/EmployeeController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/controllers/EmployeeController.java
@@ -3,10 +3,9 @@ package com.example.cruddemo.controllers;
 import com.example.cruddemo.entity.Employee;
 import com.example.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,27 @@ public class EmployeeController {
     @GetMapping("/employees")
     public ResponseEntity<List<Employee>> findAll() {
         return ResponseEntity.ok(employeeService.findAll());
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> findById(@PathVariable int id) {
+
+        Employee employee = employeeService.findById(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + id);
+        }
+
+        return ResponseEntity.ok(employee);
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity<Employee> createEmploye(@RequestBody Employee employee) {
+        // also just in case they pass an id in JSON... set id to
+        // this is to force a save of new item... instead of updates
+        employee.setId(0);
+
+        Employee savedEmployee = employeeService.save(employee);
+        return ResponseEntity.ok(savedEmployee);
     }
 }
