a3398a9de5440c983fdaa0e2c8ffd882682871e3
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-02 16:17:03 -0300

    spring data rest

---
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/cruddemo/repository/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.cruddemo.repository;
+
+import com.example.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
