5fd74b9f353f606fc10defe65e6a6ca9f1d55ff4
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-02 16:53:05 -0300

    feat: Update application properties and enhance EmployeeRepository with REST support

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/cruddemo/repository/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/cruddemo/repository/EmployeeRepository.java
@@ -2,5 +2,7 @@ package com.example.cruddemo.repository;
 
 import com.example.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
