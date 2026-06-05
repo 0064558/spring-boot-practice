269114f346e13859a524c32ce3e011f10f301d9f
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-05 15:50:00 -0300

    feat: Add method to retrieve employees ordered by first name in EmployeeRepository

--- 07-spring-boot-spring-mvc-crud/01-spring-boot-mvc-crud-employee/src/main/java/com/example/thymeleafdemo/repository/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/01-spring-boot-mvc-crud-employee/src/main/java/com/example/thymeleafdemo/repository/EmployeeRepository.java
@@ -3,4 +3,8 @@ package com.example.thymeleafdemo.repository;
 import com.example.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByOrderByFirstNameAsc();
+}
