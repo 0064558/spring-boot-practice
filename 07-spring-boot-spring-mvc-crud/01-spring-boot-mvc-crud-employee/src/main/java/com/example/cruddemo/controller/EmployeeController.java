077fe3c5f83038e44f88de4b05df4608ba502547
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-05 13:32:37 -0300

    feat: Initialize employee spring mvc directory application with basic structure and configurations

---
+++ 07-spring-boot-spring-mvc-crud/01-spring-boot-mvc-crud-employee/src/main/java/com/example/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.example.cruddemo.controller;
+
+import com.example.cruddemo.entity.Employee;
+import com.example.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        // get the employees from db
+        List<Employee> employees = new ArrayList<>();
+        employees = employeeService.findAll();
+
+        // add to the spring model
+        model.addAttribute("employees", employees);
+
+        return "list-employees";
+    }
+}
