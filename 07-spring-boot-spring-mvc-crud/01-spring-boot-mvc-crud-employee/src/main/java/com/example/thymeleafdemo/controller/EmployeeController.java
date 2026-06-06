c9260b2a288d7f033f8a087086ea82c0400e02df
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-05 21:55:46 -0300

    feat: Implement employee update and delete functionality with form prepopulation

--- 07-spring-boot-spring-mvc-crud/01-spring-boot-mvc-crud-employee/src/main/java/com/example/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/01-spring-boot-mvc-crud-employee/src/main/java/com/example/thymeleafdemo/controller/EmployeeController.java
@@ -5,13 +5,11 @@ import com.example.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/employees")
@@ -45,10 +43,31 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+        // get the employee frin the service
+        Optional<Employee> employee = employeeService.findById(id);
+
+        // set employee in the model to prepopulate the form
+        model.addAttribute("employee", employee);
+
+        // send over to our form
+        return "employees/employee-form";
+    }
+
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.save(employee);
 
+        return "redirect:/employees/list";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id) {
+        // delete the employee
+        employeeService.deleteById(id);
+
+        // redirect to the /employees/list
         return "redirect:/employees/list";
     }
 }
