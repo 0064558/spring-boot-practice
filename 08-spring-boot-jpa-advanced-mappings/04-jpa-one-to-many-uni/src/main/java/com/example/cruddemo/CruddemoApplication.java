1323b9e8f7d61ae71ed2e340be53754c9cb6e346
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-13 09:49:37 -0300

    add 04-jpa-one-to-many-uni

---
+++ 08-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -0,0 +1,42 @@
+package com.example.cruddemo;
+
+import com.example.cruddemo.dao.AppDAO;
+import com.example.cruddemo.entity.Course;
+import com.example.cruddemo.entity.Instructor;
+import com.example.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLOutput;
+import java.util.List;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		/*Instructor instructor = new Instructor("Miserias", "Brown", "test@gmail.com");
+		InstructorDetail instructorDetail = new InstructorDetail("Muie", "http>//www.youtube.com");
+
+		Course course1 = new Course("AFSDFSD", instructor);
+		Course course2 = new Course("asfafadsfds", instructor);
+
+		instructor.add(course1);
+		instructor.add(course2);*/
+
+		Instructor instructor = new Instructor();
+
+		return runner -> {
+
+		};
+	}
+
+
+
+}
