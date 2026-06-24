3128fd96f0147bac314c5f23c13227c7b98e040d
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-24 14:54:31 -0300

    feat: Pointcut Expressions - Match Method Parameter Types

--- 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,9 +11,8 @@ public class MyDemoLoggingAspect {
 
     // let's start with an @Before advice
 
-    //@Before("execution(public void add*())")
 
-    @Before("execution(* add*())")
+    @Before("execution(* add*(com.example.aopdemo.Account))")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
     }
--- 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/example/aopdemo/dao/AccountDAO.java
+++ 09-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/example/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,7 @@
 package com.example.aopdemo.dao;
 
+import com.example.aopdemo.Account;
+
 public interface AccountDAO {
-    void addAccount();
+    void addAccount(Account account);
 }
