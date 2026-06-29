a66f14c24245bfbb7d5ef6df8504aa180d0f628c
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-06-29 13:33:35 -0300

    feat: spring-boot-unit-testing

---
+++ 11-spring-boot-unit-testing-main/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -0,0 +1,44 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DemoUtilsTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BefoeEach executes before the execution of each test method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Running @AfterEach");
+    }
+
+    @Test
+    void testEqualsAndNotEquals() {
+        System.out.println("Running test: testEqualsAndNotEquals()");
+
+
+        assertEquals(6, demoUtils.add(2, 4), "2 + 4 should equal 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 should not equal 6");
+    }
+
+    @Test
+    void testNullAndNotNull() {
+        System.out.println("Running test: testNullAndNotNull()");
+
+        String str1 = null;
+        String str2 = "lovee";
+
+        assertNull(demoUtils.checkNull(str1), "Should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Shoud be not null");
+    }
+}
