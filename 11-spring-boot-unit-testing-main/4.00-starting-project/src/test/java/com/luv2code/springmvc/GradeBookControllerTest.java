7e0a7d95bb1b4145b509101b642cf9cae1f784ea
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-07-10 15:05:53 -0300

    feat: add test for retrieving student information in GradeBookControllerTest

--- 11-spring-boot-unit-testing-main/4.00-starting-project/src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
+++ 11-spring-boot-unit-testing-main/4.00-starting-project/src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
@@ -26,6 +26,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -191,6 +193,21 @@ public class GradeBookControllerTest {
                 .andExpect(status().is4xxClientError())
                 .andExpect(jsonPath("$.status", is(404)))
                 .andExpect(jsonPath("$.message", is("Student or Grade was not found")));
+    }
+
+    @Test
+    public void studentInformationHttpRequest() throws Exception {
+        Optional<CollegeStudent> student = studentDao.findById(1);
+
+        assertTrue(student.isPresent(), "Student should be present in the database");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/studentInformation/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(String.valueOf(APPLICATION_JSON_UTF8)))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstname", is("Eric")))
+                .andExpect(jsonPath("$.lastname", is("Roby")))
+                .andExpect(jsonPath("$.emailAddress", is("eric.roby@luv2code_school.com")));
     }
 
     // Limpeza do banco de dados após cada teste, removendo os dados inseridos durante a configuração
